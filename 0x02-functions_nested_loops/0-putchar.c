964d544782cb26ebf0637f9df8639117848353f8
SaviourMadeit <senamdagadusaviour@gmail.com>
2023-02-22 00:32:33 -0800

    updates

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,19 @@
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	char *c = "_putchar";
-	char c[] = "_putchar";
-	int i = 0;
+	char s[] = "_putchar\n";
+	int i;
 
-	for (i = 0; i < 10; i++)
-	while (c[i] != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		_putchar(c[i]);
-		i++;
+		_putchar(s[i]);
 	}
-	_putchar('\n');
 
 	return (0);
 }
