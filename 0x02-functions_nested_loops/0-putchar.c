80b022a363a7308d70f5bec461de6aa0dd052783
SaviourMadeit <senamdagadusaviour@gmail.com>
2023-02-21 23:41:36 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,10 @@
-#include "main.h"
-/**
- * main - printS  _putchar
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
+	char *c = "_putchar";
 	char c[] = "_putchar";
 	int i = 0;
 
+	for (i = 0; i < 10; i++)
 	while (c[i] != '\0')
 	{
 		_putchar(c[i]);
